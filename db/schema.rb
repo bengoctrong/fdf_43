7b3fdffa292266e4dd761d7aaf87c6eaa01f5406
bengoctrong <bengoctrong@gmail.com>
2018-06-22 15:37:08 +0700

    add product with images

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180621031921) do
+ActiveRecord::Schema.define(version: 20180622074730) do
 
   create_table "categories", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "name"
@@ -53,6 +53,7 @@ ActiveRecord::Schema.define(version: 20180621031921) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "is_delete", default: 0
+    t.json "images"
     t.index ["category_id"], name: "index_products_on_category_id"
   end
 
